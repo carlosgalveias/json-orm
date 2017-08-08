016208c1fac3e5a58419393d092240fb0c278568
Carlos Galveias <carlos.galveias@pt.ibm.com>
2017-08-08 10:43:55 +0100

    added findSync

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ function jsonorm(json) {
    * @param  {Object} query The search query
    * @return {Array} Array of path references to the objects
    */
-  this.find = function (query) {
-    return new Promise((resolve, reject) => {
-      if (!this.data) {
-        var stack = JSON.stringify(this);
-        reject("You need to load the json first\n" + stack);
-      }
+  this.findSync = function (query) {
+    if (!this.data) {
+      var stack = JSON.stringify(this);
+      throw (new Error("You need to load the json first\n" + stack));
       if (query.and) {
         // TODO , find objects that match >1 query within
         let foundQueries = [];
@@ -41,7 +39,7 @@ function jsonorm(json) {
             matchedObjects.push(f);
           }
         });
-        return resolve(matchedObjects);
+        return matchedObjects;
       } else if (query.or) {
         // TODO , find objects that match >1 query within
         let foundQueries = [];
@@ -61,7 +59,7 @@ function jsonorm(json) {
             matchedObjects.push(f);
           }
         });
-        return resolve(matchedObjects);
+        return matchedObjects;
       }
       if (query.length) {
         for (let n in query) {
@@ -70,98 +68,106 @@ function jsonorm(json) {
       } else {
         this.foundObjects = _find(this.data, query);
       }
-      resolve(this.foundObjects);
-    });
-  };
-  /**
-   * Updates the object with the new object
-   * @param  {String} path   The object path to update
-   * @param  {Object} newObj The new object/value
-   * @return {Boolean}       Boolean return
-   */
-  this.update = function (path, newObj) {
-    return new Promise((resolve, reject) => {
+      return this.foundObjects;
+    };
+    this.find = function (query) {
+      return new Promise((resolve, reject) => {
+        try {
+          resolve(this.findSync(query));
+        } catch (e) {
+          reject(e);
+        }
+      });
+    };
+    /**
+     * Updates the object with the new object
+     * @param  {String} path   The object path to update
+     * @param  {Object} newObj The new object/value
+     * @return {Boolean}       Boolean return
+     */
+    this.update = function (path, newObj) {
+      return new Promise((resolve, reject) => {
+        if (!this.data) {
+          reject("You need to load the json first");
+        }
+        if (typeof (path) === "object" && path.length > 0) {
+          path.forEach(p => {
+            this.data = _update(this.data, p, newObj);
+          });
+        } else {
+          this.data = _update(this.data, path, newObj);
+        }
+        resolve();
+      });
+    };
+    /**
+     * Inserts a new object before or after our path object
+     * @param  {String} path   The path for our reference object
+     * @param  {Object} newObj The new Object to insert
+     * @param  {Boolean} before Before or After (default)
+     * @return {Boolean} If insert is successfull
+     */
+    this.insert = function (path, newObj, before) {
+      return new Promise((resolve, reject) => {
+        if (!this.data) {
+          reject("You need to load the json first");
+        }
+        if (typeof (path) === "object" && path.length > 0) {
+          path.forEach(p => {
+            this.data = _insert(this.data, p, newObj, before);
+          });
+        } else {
+          this.data = _insert(this.data, path, newObj, before);
+        }
+        resolve();
+      });
+    };
+    /**
+     * Inserts a new object before or after our path object
+     * @param  {String} path   The path for our reference object
+     * @param  {Object} newObj The new Object to insert
+     * @param  {Boolean} before Before or After (default)
+     * @return {Boolean} If insert is successfull
+     */
+    this.remove = function (path) {
+      return new Promise((resolve, reject) => {
+        if (!this.data) {
+          reject("You need to load the json first");
+        }
+        if (typeof (path) === "object" && path.length > 0) {
+          path.forEach(p => {
+            _remove(this.data, p);
+          });
+        } else {
+          _remove(this.data, path);
+        }
+        resolve();
+      });
+    };
+    /**
+     * Gets the json object from a given path
+     * @param  {String} path Path for the object
+     * @return {Object}      The object
+     */
+    this.getObject = function (path) {
       if (!this.data) {
-        reject("You need to load the json first");
+        throw ("You need to load the json first");
       }
-      if (typeof (path) === "object" && path.length > 0) {
-        path.forEach(p => {
-          this.data = _update(this.data, p, newObj);
-        });
-      } else {
-        this.data = _update(this.data, path, newObj);
-      }
-      resolve();
-    });
-  };
-  /**
-   * Inserts a new object before or after our path object
-   * @param  {String} path   The path for our reference object
-   * @param  {Object} newObj The new Object to insert
-   * @param  {Boolean} before Before or After (default)
-   * @return {Boolean} If insert is successfull
-   */
-  this.insert = function (path, newObj, before) {
-    return new Promise((resolve, reject) => {
+      return common.getObjectFromPath(this.data, path);
+    };
+    this.setObject = function (path, object) {
       if (!this.data) {
-        reject("You need to load the json first");
+        throw ("You need to load the json first");
       }
-      if (typeof (path) === "object" && path.length > 0) {
-        path.forEach(p => {
-          this.data = _insert(this.data, p, newObj, before);
-        });
-      } else {
-        this.data = _insert(this.data, path, newObj, before);
+      if (!path) {
+        throw ("Cannot set object without path");
       }
-      resolve();
-    });
-  };
-  /**
-   * Inserts a new object before or after our path object
-   * @param  {String} path   The path for our reference object
-   * @param  {Object} newObj The new Object to insert
-   * @param  {Boolean} before Before or After (default)
-   * @return {Boolean} If insert is successfull
-   */
-  this.remove = function (path) {
-    return new Promise((resolve, reject) => {
-      if (!this.data) {
-        reject("You need to load the json first");
-      }
-      if (typeof (path) === "object" && path.length > 0) {
-        path.forEach(p => {
-          _remove(this.data, p);
-        });
-      } else {
-        _remove(this.data, path);
-      }
-      resolve();
-    });
-  };
-  /**
-   * Gets the json object from a given path
-   * @param  {String} path Path for the object
-   * @return {Object}      The object
-   */
-  this.getObject = function (path) {
-    if (!this.data) {
-      throw ("You need to load the json first");
-    }
-    return common.getObjectFromPath(this.data, path);
-  };
-  this.setObject = function (path, object) {
-    if (!this.data) {
-      throw ("You need to load the json first");
-    }
-    if (!path) {
-      throw ("Cannot set object without path");
-    }
-    this.data = common.setObjectFromPath(this.data, path, object);
-    return this.data;
-  };
-  this.getParent = function (path) {
-    return common.getParent(path);
-  };
-  return this;
-}
-module.exports = jsonorm;
+      this.data = common.setObjectFromPath(this.data, path, object);
+      return this.data;
+    };
+    this.getParent = function (path) {
+      return common.getParent(path);
+    };
+    return this;
+  }
+  module.exports = jsonorm;
